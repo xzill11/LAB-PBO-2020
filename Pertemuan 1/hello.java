9fbcb22a9b4c5e5590081eb18bd81a36e44e3264
Fazil Basri <fazillbasry@gmail.com>
2020-10-02 17:04:06 +0700

    Membuat kode pertemuan 1

---
+++ Pertemuan 1/hello.java
@@ -0,0 +1,10 @@
+class hello{
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
+
+/*
+ Kde ini sebagai percobaan menggunakan git..
+ kode ini menampilakn kata hello
+*/
